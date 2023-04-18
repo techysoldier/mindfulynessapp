260e836ac40278f2a4340d9621829ac34aa61cda
devin <devinsymone@gmail.com>
2023-04-17 20:27:50 -0400

    adjust app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 // import Home from "./Pages/Home";
-// import Login from "./Pages/Login";
+import Login from "./Pages/Login";
 // import Profile from "./Pages/Profile";
 // import Register from "./Pages/Register";
 
 
 
-import Register from "./Pages/Register";
-
 function App() {
-  return <Register/>;
+  return <Login/>;
 }
 
 export default App;
